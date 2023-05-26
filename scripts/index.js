e2ba8088bdeb0b9a642bcd7b5ef129eadc5710a4
Juliano Souza <souzza.s1990@gmail.com>
2023-05-25 22:23:52 -0300

    fix link

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-import { CalendarView } from "./CalendarView.js";
+import { CalendarView } from "./calendarView.js";
 import { CalendarController } from "./calendarController.js";
 const calendarView = new CalendarView()
 const calendarController = new CalendarController({calendarView: calendarView})
